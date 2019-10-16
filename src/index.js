8e0046c86a209d4ad4a98332b2d4c3b00b7fe90c
jamesncox <jamesncox@yahoo.com>
2019-10-16 11:15:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+function fetchDogs() {
+  return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => renderDogs(json));
+}
+
+function renderDogs(json) {
+  const dogImageContainer = document.querySelector("dog-image-container");
+  json.forEach(dog => {
+    const img = document.createElement("img");
+    img.innerHTML = "Image Added";
+    dogImageContainer.appendChild(img);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetchDogs();
+});
